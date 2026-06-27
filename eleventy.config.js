1c303c102a460b21180918bc089c0abed9fd991a
Jonathan Chin <jonathan.chin@posteo.net>
2026-06-27 12:13:48 -0400

    feat: add static img

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,9 +1,13 @@
+import { eleventyImageTransformPlugin } from '@11ty/eleventy-img';
+
 export default function (eleventyConfig) {
   eleventyConfig.setInputDirectory('./src');
   eleventyConfig.setOutputDirectory('./dist');
 
   eleventyConfig.addPassthroughCopy("./src/css/");
   eleventyConfig.addWatchTarget("./src/css/");
+
+  eleventyConfig.addPlugin(eleventyImageTransformPlugin);
 }
 
 export const config = {
